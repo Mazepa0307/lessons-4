3fa5e7513f6e94291355d238ca3a21545cc0f9ad
Mazepa0307 <danekmazepa@gmail.com>
2024-08-11 17:32:27 +0200

    Update lesson4.py3.py

--- lesson4.py3.py
+++ lesson4.py3.py
@@ -1,6 +1,5 @@
 import random
 lst = [x for x in range(random.randint (3,10))]
-new_lst = []
-new_lst.append([lst[0],lst[2],lst[-2]])
+new_lst = [lst[0], lst[2], lst[-2]]
 print(lst)
 print(new_lst)
